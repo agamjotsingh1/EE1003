9acaa6d3883abb7cf1caead6cff95b9f539a6e92
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2025-01-28 22:41:54 +0530

    11.16.3.8.9 revision

--- prob/ncert/11.16.3.8.9/codes/cdf.py
+++ prob/ncert/11.16.3.8.9/codes/cdf.py
@@ -1,25 +1,53 @@
 import matplotlib.pyplot as plt
+from scipy.stats import binom 
+from math import sqrt, pi
 import numpy as np
+import ctypes
 
-# CDF piecewise function
-def cdf(n):
-    if n < 0:
-        return 0
-    elif 0 <= n < 1:
-        return 0.125
-    elif 1 <= n < 2:
-        return 0.5
-    elif 2 <= n < 3:
-        return 0.875
-    else:
-        return 1
+''' THEORITICAL '''
+# returns the theoritical binomial cdf distribution
+def cdf_binomial(n, p):
+    # defining the list of x values 
+    x = np.arange(0, n + 1)
 
-x = np.linspace(0, 4, 1000)
-y = [cdf(n) for n in x]
+    # list of cdf values 
+    y = binom.cdf(x, n, p)
 
-# Plotting CDF
-plt.plot(x, y, label="Cummulative Distribution Function $F_{X}(n)$")
+    return x, y
 
+n = 3 # number of tosses
+p = 0.5 # probability of heads
+
+x, y = cdf_binomial(n, p) # get the probability distribution
+
+''' SIMULATION '''
+# dll linking
+dll = ctypes.CDLL('./sim.so')
+
+# describing the argument and return types of the function 'binomCdfPlot' and 'freeSingleMem' in the dll
+dll.binomCdfPlot.argtypes = [ctypes.c_int]*2 + [ctypes.c_float]
+dll.binomCdfPlot.restype = ctypes.POINTER(ctypes.c_float)
+
+dll.freeSingleMem.argtypes = [ctypes.POINTER(ctypes.c_float)]
+dll.freeSingleMem.restype = None
+
+iters = 100000
+simcdf = dll.binomCdfPlot(n, iters, p)
+
+# plotting the plot using plt.scatter
+coords = []
+for pt in zip(range(0, n + 1), simcdf[:(n + 1)]):
+    coords.append(np.array([[pt[0], pt[1]]]).reshape(-1, 1))
+
+coords_plot = np.block(coords)
+
+# freeing the memory of simcdf
+dll.freeSingleMem(simcdf)
+
+# Show the thoeritical and simulated cdf
+markerline, stemlines, _ = plt.stem(coords_plot[0,:], coords_plot[1,:], 'red', label = "Sim") 
+plt.setp(markerline, 'markersize', 8)
+plt.stem(x, y, label="Theory")
 plt.legend(loc="best")
 plt.savefig("../figs/cdf.png")
 plt.show()
--- prob/ncert/11.16.3.8.9/codes/pmf.py
+++ prob/ncert/11.16.3.8.9/codes/pmf.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 from scipy.stats import binom 
 from math import sqrt, pi
 import numpy as np
+import ctypes
 
-# returns the binomial pmf distribution
+''' THEORITICAL '''
+# returns the theoritical binomial pmf distribution
 def pmf_binomial(n, p):
     # defining the list of x values 
     x = np.arange(0, n + 1)
@@ -18,13 +20,40 @@ p = 0.5 # probability of heads
 
 x, y = pmf_binomial(n, p) # get the probability distribution
 
-# Plot the pmf
+''' SIMULATION '''
+# dll linking
+dll = ctypes.CDLL('./sim.so')
+
+# describing the argument and return types of the function 'binomPmfPlot' and 'freeSingleMem' in the dll
+dll.binomPmfPlot.argtypes = [ctypes.c_int]*2 + [ctypes.c_float]
+dll.binomPmfPlot.restype = ctypes.POINTER(ctypes.c_float)
+
+dll.freeSingleMem.argtypes = [ctypes.POINTER(ctypes.c_float)]
+dll.freeSingleMem.restype = None
+
+iters = 100000
+simPmf = dll.binomPmfPlot(n, iters, p)
+
+# plotting the plot using plt.scatter
+coords = []
+for pt in zip(range(0, n + 1), simPmf[:(n + 1)]):
+    coords.append(np.array([[pt[0], pt[1]]]).reshape(-1, 1))
+
+coords_plot = np.block(coords)
+
+# freeing the memory of simPmf
+dll.freeSingleMem(simPmf)
+
+# Show the thoeritical and simulated pmf
 plt.figure()
-plt.stem(x, y, label="PMF $P_{X}(n)$")
+markerline, stemlines, _ = plt.stem(coords_plot[0,:], coords_plot[1,:], 'red', label = "Sim") 
+plt.setp(markerline, 'markersize', 8)
+plt.stem(x, y, label="Theory")
 plt.legend(loc="upper right")
 plt.savefig("../figs/pmf1.png")
 plt.show()
 
+''' GAUSSIAN DISTRIBUTION '''
 n = 15
 p = 0.5
 
