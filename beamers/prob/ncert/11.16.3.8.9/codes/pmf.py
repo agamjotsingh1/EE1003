9a91cd44920972f76640cd77e01e78aec76b5429
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2025-01-29 23:49:54 +0530

    beamer 11.16.3.8.9 final

--- beamers/prob/ncert/11.16.3.8.9/codes/pmf.py
+++ beamers/prob/ncert/11.16.3.8.9/codes/pmf.py
@@ -50,26 +50,5 @@ markerline, stemlines, _ = plt.stem(coords_plot[0,:], coords_plot[1,:], 'red', l
 plt.setp(markerline, 'markersize', 8)
 plt.stem(x, y, label="Theory")
 plt.legend(loc="upper right")
-plt.savefig("../figs/pmf1.png")
-plt.show()
-
-''' GAUSSIAN DISTRIBUTION '''
-n = 15
-p = 0.5
-
-mean = n * p
-variance = n * p * (1 - p)
-
-# plotting gaussian distribution and pmf for n = 10
-x, y = pmf_binomial(n, p)
-plt.figure()
-plt.stem(x, y)
-
-x_lin = np.linspace(0, n, 1000)
-y_lin = np.exp(-np.power(x_lin - mean, 2)/(2 * variance))/sqrt(2*pi*variance)
-
-plt.plot(x_lin, y_lin, color="orange", label="Gaussian distribution m = 15")
-
-plt.legend(loc="upper right")
-plt.savefig("../figs/pmf2.png")
+plt.savefig("../figs/pmf.png")
 plt.show()
