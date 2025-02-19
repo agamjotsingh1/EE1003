8d0df4e6075cad7b8bedd81a19e2285044b3fc33
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2025-02-20 00:50:25 +0530

    hardware arduino flipflop inc FINALLY DONE

--- hardware/assembly/flipflops_inc/flipflops.asm
+++ hardware/assembly/flipflops_inc/flipflops.asm
@@ -8,19 +8,148 @@ out DDRB, r16 ; declaring pins as input in register B
 
 ;sbi DDRB, 5 ;set pin 13 as output pin (DDRB pin 5)
 ldi r16, 0b00000101 ;the last 3 bits define the prescaler, 101 => division by 1024
-out TCCR0B, r16 
-;prescalar used = 1024. So new freq. of clock cycle = (16 MHz / 1024) = 16 kHz
+out TCCR0B, r16 ;prescalar used = 1024. So new freq. of clock cycle = (16 MHz / 1024) = 16 kHz
 
-clr r18 ;output bits. we are only interested in bit 6 from the right.
+clr r28 ;output bits. we are only interested in bit 6 from the right.
 
-ldi r20,0b00100000	;initializing
+.def W = r17
+.def X = r18
+.def Y = r15
+.def Z = r20
+
+.def A = r21
+.def B = r22
+.def C = r23
+.def D = r24
+
+.def ONE = r30
+.def TEMP = r16
+.def TEMP2 = r25
+.def TEMP3 = r26
+.def TEMP4 = r27
+
+ldi ONE, 0b00000001
+clr TEMP
+clr TEMP2
+clr TEMP3
+clr TEMP4
 
 loop:
-  ldi r25, 0b00000100
-	out PORTD, r25
+  in TEMP, PINB
 
-	eor r18, r20 		;change the output of LED
-	out PORTB, r18 
+  mov W, TEMP
+  and W, ONE
+
+  lsr TEMP
+  mov X, TEMP
+  and X, ONE
+
+  lsr TEMP
+  mov Z, TEMP
+  and Z, ONE
+
+  lsr TEMP
+  mov Y, TEMP
+  and Y, ONE
+  
+; A
+  mov TEMP, W
+  eor TEMP, ONE
+  mov A, TEMP
+
+; B
+  ; !W & X & !Z
+  mov TEMP, W
+  eor TEMP, ONE
+  and TEMP, X
+  mov TEMP2, Z
+  eor TEMP2, ONE
+  and TEMP, TEMP2
+
+  ; W & !X & !Z
+  mov TEMP2, X
+  eor TEMP2, ONE
+  and TEMP2, W
+  mov TEMP3, Z
+  eor TEMP3, ONE
+  and TEMP2, TEMP3
+
+  or TEMP, TEMP2
+  mov B, TEMP
+
+; C
+  ; !W & Y & !Z
+  mov TEMP, W
+  eor TEMP, ONE
+  and TEMP, Y
+  mov TEMP2, Z
+  eor TEMP2, ONE
+  and TEMP, TEMP2
+
+  ; !X & Y & !Z
+  mov TEMP2, X
+  eor TEMP2, ONE
+  and TEMP2, Y
+  mov TEMP3, Z
+  eor TEMP3, ONE
+  and TEMP2, TEMP3
+
+  ; W & X & !Y & !Z
+  mov TEMP3, Y
+  eor TEMP3, ONE
+  and TEMP3, W
+  and TEMP3, X
+  mov TEMP4, Z
+  eor TEMP4, ONE
+  and TEMP3, TEMP4
+
+  or TEMP, TEMP2
+  or TEMP, TEMP3
+  mov C, TEMP
+
+; D
+  ; W & X & Y & !Z
+  mov TEMP, Z
+  eor TEMP, ONE
+  and TEMP, X
+  and TEMP, Y
+  and TEMP, W
+
+  ; !W & !X & !Y & Z
+  mov TEMP2, W
+  eor TEMP2, ONE
+  and TEMP2, Z
+  mov TEMP3, X
+  eor TEMP3, ONE
+  and TEMP2, TEMP3
+  mov TEMP3, Y
+  eor TEMP3, ONE
+  and TEMP2, TEMP3
+
+  or TEMP, TEMP2
+  mov D, TEMP
+
+  lsl B
+
+  lsl C
+  lsl C
+
+  lsl D
+  lsl D
+  lsl D
+
+  mov TEMP, A
+  add TEMP, B
+  add TEMP, C
+  add TEMP, D
+  lsl TEMP
+  lsl TEMP
+
+	out PORTD, TEMP
+
+  ldi TEMP, 0b00100000	;initializing
+	eor r28, TEMP ;change the output of LED
+	out PORTB, r28 
 
 	ldi r19, 0b01000000 ;times to run the loop = 64 for 1 second delay
 	rcall PAUSE 		;call the PAUSE label
@@ -39,3 +168,4 @@ lp2:	;loop runs 64 times
 
 Start:
   rjmp Start
+
